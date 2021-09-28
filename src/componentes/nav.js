3bfc42d1d2079c17af1900f22c608dee49aa0bba
ignacio vera <kyfaxsama@gmail.com>
2021-09-28 16:15:12 -0300

    asd

---
+++ src/componentes/nav.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+const nav=()=>{
+
+    return(
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+
+    )
+
+
+}
+
+
+export default nav;
