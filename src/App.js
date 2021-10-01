04a4afb51171717b01663b8cc435f0c175a22b4e
ignacio vera <kyfaxsama@gmail.com>
2021-10-01 20:00:11 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 // import Nav from "./componentes/header/nav.js";
-import Header from "./componentes/header/header.js";
+import Header from "./componentes/header/Header.js";
 import Lista from "./componentes/items/itemsListContainer.js";
-import Alert from "./componentes/body/alert.js";
+// import Alert from "./componentes/body/alert.js";
 
 function App() {
   return (
--- src/componentes/header/header.js
+++ src/componentes/header/header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Nav from "./Nav.js";
 import "./HeaderEstilos.css";
-import Carrito from "../carrito/CartWidget.js";
 
 const Header = () => {
   return (
