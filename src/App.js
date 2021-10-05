8d408507b47c21c06c44fa5084fae29ad1c21018
ignacio vera <kyfaxsama@gmail.com>
2021-10-05 20:07:54 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./componentes/header/Header.js";
 import Lista from "./componentes/items/itemsListContainer.js";
 // import Alert from "./componentes/body/alert.js";
+import Contador from "./componentes/body/Contador.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Lista props="Productos" />
+      <Contador stock="7">ASD</Contador>
     </div>
   );
 }
